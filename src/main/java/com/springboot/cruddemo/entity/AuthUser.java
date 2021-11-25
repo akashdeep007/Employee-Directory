5158a5befe99a83f78587ba4492318af5332c8ac
akashdeep007 <beingakscool@gmail.com>
2021-11-25 21:08:39 +0530

    On login returning jwt

---
+++ src/main/java/com/springboot/cruddemo/entity/AuthUser.java
@@ -0,0 +1,34 @@
+package com.springboot.cruddemo.entity;
+
+public class AuthUser {
+
+	private String username;
+	private String password;
+
+	public AuthUser() {
+
+	}
+
+	public AuthUser(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- src/main/java/com/springboot/cruddemo/security/SecurityConfigurer.java
+++ src/main/java/com/springboot/cruddemo/security/SecurityConfigurer.java
@@ -2,6 +2,7 @@ package com.springboot.cruddemo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,26 +12,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.springboot.cruddemo.service.MyUserDetailsService;
 
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
 
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	    return super.authenticationManagerBean();
+	}
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService);
 	}
-	
+
 	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
+	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/api/login/**").permitAll().anyRequest().authenticated();
+	}
 
 }
